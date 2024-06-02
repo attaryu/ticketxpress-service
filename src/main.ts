40c78accdd79b9e4e99adc490ea81b2f188c5e4f
Attar <mattarannaufal@gmail.com>
2024-06-02 13:11:39 +0700

    fix: cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
 
 import checkToken from './middleware/checkToken';
 import router from './router';
@@ -9,6 +10,10 @@ import checkAdminRoute from './middleware/checkAdmin';
 
 const app = express();
 
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
